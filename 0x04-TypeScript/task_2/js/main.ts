80410df6855aa13686f72036721f8860c801cc3b
CornellsWorld <corneliusalhassan@gmail.com>
2025-11-09 14:05:53 +0100

    update1

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -134,3 +134,18 @@ console.log(executeWork(createEmployee(1000)));  // Getting to director tasks
 
 
 
+// Define a string literal type
+type Subjects = "Math" | "History";
+
+// Define the function teachClass
+function teachClass(todayClass: Subjects): string {
+  if (todayClass === "Math") {
+    return "Teaching Math";
+  } else if (todayClass === "History") {
+    return "Teaching History";
+  }
+}
+
+// Example usage:
+console.log(teachClass("Math"));     // Teaching Math
+console.log(teachClass("History"));  // Teaching History
